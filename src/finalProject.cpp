2a761316892a30f4ba59dee804ea4f3ee7a1517a
Samuel Lamontagne <samuel.lamontagne.86@gmail.com>
2021-03-12 16:37:12 -0500

    Trying to do the graph

--- src/finalProject.cpp
+++ src/finalProject.cpp
@@ -7,9 +7,35 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
+#include "initGraph.h"
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	vector<Graphics> lvl1[3];
+	Graphics one = straightLine();
+	Graphics two = straightLine();
+	Graphics three = endRoom();
+	Graphics town1 = town();
+
+	char output;
+
+	lvl1[two].push_back(one);
+	lvl1[one].push_back(two);
+	lvl1[two].push_back(three);
+	lvl1[three].push_back(two);
+
+	one.PrintDisplay();
+	cout << "do you want to continue ? y/n" << endl;
+	cin >> output;
+	//Clear the real terminal screen
+	//For linux
+	system("clear");
+	//For windows
+	//system("CLS");
+	town1.PrintDisplay();
+
 	return 0;
 }
