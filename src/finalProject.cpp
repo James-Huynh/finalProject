87ad4a9afa302b8ae0ae535a88dd2e61453394d0
lazy-fishman <qin.shao.ca@gmail.com>
2021-03-18 08:43:22 +0800

    file write and load completed test firlOperation is passed.

--- src/finalProject.cpp
+++ src/finalProject.cpp
@@ -47,6 +47,7 @@ void testEquipment() {
     kingSam.equipSecWeapon(myShield);
 
     kingSam.printEquipment();
+    kingSam.printCharacter();
     cout << endl;
 }
 
@@ -62,33 +63,33 @@ void testRoles() {
 }
 
 void testCombat() {
-	Role::initializeRoles();
+    Role::initializeRoles();
 
-	Character kingJames("James", '1');
-	Character queenSam("Sam", '1');
+    Character kingJames("James", '1');
+    Character queenSam("Sam", '1');
 
-	Weapon* mySword = new Sword("wood sword","level 1 with 5% pierce through armor",100,1,100,0.55);
-	Weapon* myArmor = new Armor("cloth armor ","level 2 with 10% against the damage",500,2,180,0.1);	// testing values
+    Weapon *mySword = new Sword("wood sword", "level 1 with 5% pierce through armor", 100, 1, 100, 0.55);
+    Weapon *myArmor = new Armor("cloth armor ", "level 2 with 10% against the damage", 500, 2, 80,
+                                0.1);    // testing values
 
-	kingJames.equipMainWeapon(mySword);
-	queenSam.equipArmor(myArmor);
+    kingJames.equipMainWeapon(mySword);
+    queenSam.equipArmor(myArmor);
 
-	// This is physical abuse
-	cout << "Initial HP of Sam: " << queenSam.getHp() << endl;
-	for (int i = 0; i < 10; ++i) {
-		cout << "Round: " << i+1 << endl;
-		kingJames.basicAttack(&queenSam);
-		cout << "HP of Sam: " << queenSam.getHp() << endl << endl;
-	}
-
+    // This is physical abuse
+    cout << "Initial HP of Sam: " << queenSam.getHp() << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << "Round: " << i + 1 << endl;
+        kingJames.basicAttack(&queenSam);
+        cout << "HP of Sam: " << queenSam.getHp() << endl << endl;
+    }
 
 }
+
 
 void entityMain() {
     testRoles();
     cout << endl;
     testCharacter();
-    cout << endl;
     testEquipment();
     cout << endl;
     testCombat();
@@ -96,60 +97,80 @@ void entityMain() {
 //END of James' part
 
 
-/*------------------------ qin--------------------------*/
-void qinMain(){
-    Merchandise merchandise("Depanneur711", "weapons and potions for sale");
-    map<Item *, int> saleList;
+/*-------------------qin----------------------*/
 
-    vector<Item *> itemListTotal = getTotalListItem();
+void gameSaveAndLoad() {
+    vector<Item *> totalListItem = getTotalListItem();
+    Merchandise merchandise("hello world", "this is for test",totalListItem);
+    Character kingSam("tom");
+    int level_i = 1;
+    Weapon *mySword = new Sword("iron sword", "level 2 with 20% pierce through armor", 500, 2, 500, 0.65);
+    Weapon *myArmor = new Armor("iron armor", "level 3 with 20% against the damage", 1000, 3, 700, 0.2);
+    Weapon *myShield = new Shield("iron shield", "level 2 with 20% pierce through armor", 500, 2, 500, 0.10);
+
+    kingSam.equipMainWeapon(mySword);
+    kingSam.equipArmor(myArmor);
+    kingSam.equipSecWeapon(myShield);
+
+    Inventory inventory("default bag");
+
+    inventory.addItem(totalListItem.at(17), 1);
+    inventory.addItem(totalListItem.at(2), 1);
+    inventory.addItem(totalListItem.at(3), 1);
+    inventory.addItem(totalListItem.at(15), 1);
+    kingSam.setMyInventory(inventory);
+
+    FileOperation::saveGame(kingSam, level_i, merchandise);
+    Merchandise m("711", "this is for test");
+    Character c("tom");
+    int level_j;
+
+    FileOperation::loadGame("myFile2.txt", c, m, level_j);
+    c.getMyEquipment().printEquipment();
+    c.getMyInventory().showPotions();
+    c.getMyInventory().showWeapons();
+
+    cout << level_j<<endl;
+
+    m.printInfo();
 
 
-    Inventory inventory("default Bag");
-    for(int i =0 ; i <20 ; i+=3)
-        inventory.addItem(itemListTotal.at(i),1);
 
-    inventory.addItem(itemListTotal.at(18),3);
-
-
-    inventory.showPotions();
-    inventory.showMyMoney();
-    inventory.showWeapons();
 }
-/*------------------------finish--------------------------*/
 
 
 int main() {
 
-	//For random values
-	srand (time(NULL));
-	//Initialize the roles
-	Role::initializeRoles();
+    //For random values
+    srand(time(NULL));
+    //Initialize the roles
+    Role::initializeRoles();
 
-	//Sam's part
+    //Sam's part
 
-	//mainGameplay();
+    //mainGameplay();
 
-	//END of Sam's part
-
-
-
-	//James' part
-
-	//entityMain();
-
-	//END of James' part
+    //END of Sam's part
 
 
 
-	//Qin' part
+    //James' part
+//    entityMain();
 
-	//END of Qin's part
+    //END of James' part
 
 
-	/*
-	for (auto & i : itemListTotal){
+
+    //Qin' part
+
+    gameSaveAndLoad();
+    //END of Qin's part
+
+
+    /*
+    for (auto & i : itemListTotal){
         delete i;
     }
-	 */
-	return 0;
+     */
+    return 0;
 }
